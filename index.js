913a5250bcd5daecedddfb15ac07f3f07a240198
Koro soft <koroku@wkmt.jp>
2022-03-31 19:59:02 +0900

    make it is

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.]
+    intents: [Discord.Intents.FLAGS.GUILDS]
 });
+const { token } = require('./auth');
 
 client.on('ready', function () {
-    console.log('client is ready');
+    client.application.commands.set([{
+        name: "music",
+        description: "Play audio on voice channels",
+        options: [{
+            type: "STRING",
+            name: "resource",
+            description: "What to play ('file'/YouTube url/url)",
+            required: true
+        }, {
+            type: "NUMBER",
+            name: "volume",
+            description: "The volume to play. The existing value is 100.",
+            required: false
+        }, {
+            type: "BOOLEAN",
+            name: "loop",
+            description: "Whether to loop the music"
+        }]
+    }, {
+        name: "stopmusic",
+        description: "Stop playing music"
+    }]).then(function () {
+        console.log('client is ready');
+    });
 });
+client.on('interactionCreate', function (interaction) {
+    if (interaction.isCommand()) {
+        if (interaction.command.name == 'music') {
+            
+        }
+    }
+})
 
-client.login();
+client.login(token);
