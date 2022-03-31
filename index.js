b37f09e397382939b8bcb04a9f27c8e9951d8937
Koro soft <koroku@wkmt.jp>
2022-03-31 23:50:49 +0900

    make it is

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.GUILDS]
+    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_VOICE_STATES]
 });
+const voice = require('@discordjs/voice');
 const { token } = require('./auth');
 
 client.on('ready', function () {
@@ -11,29 +13,69 @@ client.on('ready', function () {
         options: [{
             type: "STRING",
             name: "resource",
-            description: "What to play ('file'/YouTube url/url)",
+            description: "What to play (YouTube url/url)",
             required: true
         }, {
             type: "NUMBER",
             name: "volume",
             description: "The volume to play. The existing value is 100.",
             required: false
-        }, {
-            type: "BOOLEAN",
-            name: "loop",
-            description: "Whether to loop the music"
         }]
-    }, {
-        name: "stopmusic",
-        description: "Stop playing music"
     }]).then(function () {
         console.log('client is ready');
     });
 });
-client.on('interactionCreate', function (interaction) {
+
+client.on('interactionCreate', async function (interaction) {
     if (interaction.isCommand()) {
         if (interaction.command.name == 'music') {
-            
+            await interaction.reply({ content: 'joining voice channel...', ephemeral: true });
+            if (interaction.member.voice.channel) {
+                if (interaction.member.voice.channel.joinable && interaction.member.voice.channel.speakable) {
+                    const conn = voice.joinVoiceChannel({
+                        adapterCreator: interaction.guild.voiceAdapterCreator,
+                        channelId: interaction.member.voice.channelId,
+                        guildId: interaction.guild.id,
+                        selfDeaf: true,
+                        selfMute: false
+                    });
+                    interaction.editReply('Downloading resource...');
+                    const player = voice.createAudioPlayer({
+                        behaviors: {
+                            noSubscriber: voice.NoSubscriberBehavior.Pause,
+                        }
+                    });
+                    conn.subscribe(player);
+                    const url = interaction.options.getString('resource');
+                    let resource
+                    if (ytdl.validateURL(url)) {
+                        const stream = ytdl(url, {
+                            filter: format => format.audioCodec === 'opus' && format.container === 'webm',
+                            quality: 'highest',
+                            highWaterMark: 32 * 1024 * 1024,
+                            format: 'audioonly',
+                        });
+                        resource = voice.createAudioResource(stream, {
+                            inputType: voice.StreamType.WebmOpus
+                        });
+                    } else {
+                        resource = voice.createAudioResource(url, {
+                            inputType: voice.StreamType.Arbitrary,
+                        });
+                    }
+                    
+                    player.play(resource);
+                    interaction.editReply('playing music...');
+                    await voice.entersState(player, voice.AudioPlayerStatus.Playing, 10 * 1000);
+                    await voice.entersState(player, voice.AudioPlayerStatus.Idle, 24 * 60 * 60 * 1000);
+                    interaction.editReply('end');
+                    conn.destroy();
+                } else {
+                    interaction.editReply('Unable to join audio channel');
+                }
+            } else {
+                interaction.editReply('Run while you join the voice channel');
+            }
         }
     }
 })
